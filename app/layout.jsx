02b72d4be9ddf584a301e2c119b1f828134e16d3
arrikk <horpeyhermi@gmail.com>
2023-11-30 16:43:30 +0100

    removdWidget

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,8 +15,6 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head />
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
-  <script src="https://word-ai-widet.vercel.app/widget.js" defer></script>
 			<body className="overflow-x-hidden">
 				<Providers>
 					<Root>{children}</Root>
